bb632209b4865497abd4ccc54998bcc20138ae74
Cech96 <48772593+Cech96@users.noreply.github.com>
2019-03-20 15:45:57 -0500

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,19 @@
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+//		System.out.println("HolaMundo!");
+		
+		String nombre = "cech";
+		
+		if (nombre.equals("cech")) {
+			System.out.println("Cech");
+			System.out.println("Cristhian Escudero Chuquino");
+			System.out.println("Passed");
+		}else {
+			System.out.println("Ese chull no existe");
+		}
+		
+	}
+	
+}
